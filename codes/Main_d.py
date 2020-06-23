58cef2680076700f1d7ea82bd66d850f11d8dc19
0A76D57AD1D001836A43B561B735A2F8 <0A76D57AD1D001836A43B561B735A2F8@i-search.com.cn>
2020-06-23 15:09:00 +0800

    20200623150859

--- codes/Main_d.py
+++ codes/Main_d.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# 编译日期：2020-06-23 15:05:50
+# 编译日期：2020-06-23 15:08:59
 # 版权所有：www.i-search.com.cn
 import time
 import pdb
@@ -10,9 +10,10 @@ from sys import argv
 import sys
 from ubpa.itools import rpa_import
 GlobalFun = rpa_import.import_global_fun(__file__)
+import ubpa.iie as iie
 import ubpa.iimg as iimg
+import ubpa.ikeyboard as ikeyboard
 import ubpa.iocr as iocr
-import ubpa.itools.rpa_str as rpa_str
 
 class lanhe_fapiao:
      
@@ -37,22 +38,77 @@ class lanhe_fapiao:
         code=None
         b=None
         a=None
-        #RPACodeMarked.....2314353040452
-        # 截图
-        self.__logger.debug('Flow:flow1,StepNodeTag:2314353040452,Note:')
-        a = iimg.capture_image(win_title=r'国家税务总局全国增值税发票查验平台 - Internet Explorer',win_text=r'',left_indent=1253,top_indent=572,width=210,height=36,waitfor=30)
-        # 截图
-        self.__logger.debug('Flow:flow1,StepNodeTag:2314360485954,Note:')
-        b = iimg.capture_image(win_title=r'国家税务总局全国增值税发票查验平台 - Internet Explorer',win_text=r'',left_indent=992,top_indent=620,width=139,height=64,waitfor=30)
-        # 自定义函数
-        self.__logger.debug('Flow:flow1,StepNodeTag:2314544729489,Note:')
-        tvar2314544729489 = GlobalFun.()
-        #验证码
-        self.__logger.debug('Flow:flow1,StepNodeTag:2314593469491,Note:')
-        tvar2314593469491 = code = iocr.vcode_recognize(image_path=tvar2314544729489,code_type=8001,apiKey='8159a500cc9d4a69a71e6ac14263f029',secretKey='2d078aa8c13741239b3d00ced85832e3')
-        # 输出
-        self.__logger.debug('Flow:flow1,StepNodeTag:23150518366104,Note:')
-        rpa_str.iprints(tvar2314593469491)
+        #RPACodeMarked.....231405246083
+        #网站
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314134287815,Note:')
+        iie.open_url(url='https://inv-veri.chinatax.gov.cn/index.html')
+        time.sleep(3)
+        # 鼠标点击
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314162044320,Note:')
+        iie.do_click_pos(win_title=r'国家税务总局全国增值税发票查验平台 - Internet Explorer',url=r'https://inv-veri.chinatax.gov.cn/index.html',selector=r'#fpdm',button=r'left',curson=r'center',times=1,run_mode=r'unctrl',continue_on_error=r'break',waitfor=10)
+        # 键盘输入
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314172690122,Note:输入发票代码')
+        time.sleep(0.5)
+        ikeyboard.key_send_cs(text='044031900111',waitfor=10)
+        # 键盘输入
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314184991924,Note:')
+        time.sleep(0.5)
+        ikeyboard.key_send_cs(text='{TAB}',waitfor=10)
+        # 键盘输入
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314191450626,Note:输入发票号码')
+        time.sleep(0.5)
+        ikeyboard.key_send_cs(text='23838102',waitfor=10)
+        # 键盘输入
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314202733030,Note:')
+        time.sleep(0.5)
+        ikeyboard.key_send_cs(text='{TAB}',waitfor=10)
+        # 键盘输入
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314205211432,Note:开票日期')
+        time.sleep(0.5)
+        ikeyboard.key_send_cs(text='20200611',waitfor=10)
+        # 键盘输入
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314221025834,Note:')
+        time.sleep(0.5)
+        ikeyboard.key_send_cs(text='{TAB}',waitfor=10)
+        # 图像检测
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314280051638,Note:判断输入内容')
+        tvar2314280051638 = iimg.img_exists(win_title=r'国家税务总局全国增值税发票查验平台 - Internet Explorer',img_res_path=self.path,image=r'snapshot_20200623142820581.png',fuzzy=True,confidence=0.85,waitfor=30)
+        # IF-N分支
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314270666737,Note:判断')
+        if tvar2314280051638:
+            # 键盘输入
+            self.__logger.debug('Flow:flow1,StepNodeTag:2314284845942,Note:输入校验码')
+            time.sleep(0.5)
+            ikeyboard.key_send_cs(text='916383',waitfor=10)
+        else:
+            # 键盘输入
+            self.__logger.debug('Flow:flow1,StepNodeTag:2314221967436,Note:输入开具金额')
+            time.sleep(0.5)
+            ikeyboard.key_send_cs(text='937.34',waitfor=10)
+        # While循环
+        self.__logger.debug('Flow:flow1,StepNodeTag:2314350501348,Note:')
+        while -1:
+            # 截图
+            self.__logger.debug('Flow:flow1,StepNodeTag:2314353040452,Note:')
+            a = iimg.capture_image(win_title=r'国家税务总局全国增值税发票查验平台 - Internet Explorer',win_text=r'',left_indent=1253,top_indent=572,width=210,height=36,waitfor=30)
+            # 截图
+            self.__logger.debug('Flow:flow1,StepNodeTag:2314360485954,Note:')
+            b = iimg.capture_image(win_title=r'国家税务总局全国增值税发票查验平台 - Internet Explorer',win_text=r'',left_indent=992,top_indent=620,width=139,height=64,waitfor=30)
+            # 自定义函数
+            self.__logger.debug('Flow:flow1,StepNodeTag:2314544729489,Note:')
+            tvar2314544729489 = GlobalFun.()
+            #验证码
+            self.__logger.debug('Flow:flow1,StepNodeTag:2314593469491,Note:')
+            code = iocr.vcode_recognize(image_path=tvar2314544729489,code_type=8001,apiKey='8159a500cc9d4a69a71e6ac14263f029',secretKey='2d078aa8c13741239b3d00ced85832e3')
+            time.sleep(1)
+            # 键盘输入
+            self.__logger.debug('Flow:flow1,StepNodeTag:23150732813107,Note:')
+            time.sleep(0.5)
+            ikeyboard.key_send_cs(text='{TAB}',waitfor=10)
+            # 键盘输入
+            self.__logger.debug('Flow:flow1,StepNodeTag:23150746702109,Note:')
+            time.sleep(0.5)
+            ikeyboard.key_send_cs(text=code,waitfor=10)
       
     def Main(self):
         pass
